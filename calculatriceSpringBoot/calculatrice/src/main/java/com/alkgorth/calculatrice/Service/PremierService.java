75c79455372dcc8458dc3208d1a0ea512bb226db
Alkgorth <ap.percheron@gmail.com>
2026-03-05 00:36:05 +0100

    Refacto du CSS, ajout de Premier

--- calculatriceSpringBoot/calculatrice/src/main/java/com/alkgorth/calculatrice/Service/PremierService.java
+++ calculatriceSpringBoot/calculatrice/src/main/java/com/alkgorth/calculatrice/Service/PremierService.java
@@ -1,3 +1,20 @@
+package com.alkgorth.calculatrice.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class PremierService {
-    
+
+    public boolean estPremier(int nombre) {
+        if (nombre <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(nombre); i++) {
+            if (nombre % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
